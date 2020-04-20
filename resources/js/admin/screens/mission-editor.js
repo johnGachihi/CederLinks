36c906e5ec2e8e4f44639b0df90703957c0e3c9f
johnGachihi <johngachihi3@gmail.com>
2020-04-20 10:28:58 +0300

    Add ckeditor wysiwyg editor to missions-editor page for writing mission description

--- resources/js/admin/screens/mission-editor.js
+++ resources/js/admin/screens/mission-editor.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import Navbar from "../components/navbar";
 import IconActionButton from "../components/icon-action-button";
 import ReactTooltip from "react-tooltip";
 import ImageUploader from "../components/image-uploader";
 import TextField from "../components/textfield";
 import DatePicker from "../components/datepicker"
+import CKEditor from "@ckeditor/ckeditor5-react"
+import BalloonBlockEditor from "@ckeditor/ckeditor5-build-balloon-block"
 
 function MissionEditor() {
+    let editor = useRef(null)
+    let [editorError, setEditorError] = useState(null)
+
+    if (editorError)
+        return 'Error loading editor';
+
     return (
         <div>
             <Navbar/>
             <div className="container">
                 <div className="_toolbar_">
-                    <IconActionButton icomoonIcon="icon-save" label="Save"/>
+                    <IconActionButton
+                        onClick={() => console.log(editor.current.getData())}
+                        icomoonIcon="icon-save"
+                        label="Save"
+                    />
                 </div>
 
                 <div className="row">
@@ -40,9 +52,23 @@ function MissionEditor() {
                         />
                     </div>
                 </div>
+                <div className="row" style={{marginTop: '40px'}}>
+                    <div className="col-md-8 mx-auto">
+                        <CKEditor
+                            editor={BalloonBlockEditor}
+                            config={editorConfig}
+                            onInit={_editor => editor.current = _editor}
+                            onError={error => setEditorError(error)}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
+}
+
+const editorConfig = {
+    placeholder: "Insert the mission description here",
 }
 
 export default MissionEditor
