b5269c13935d481a771a1c9625fa539c77813e0b
johnGachihi <johngachihi3@gmail.com>
2020-04-29 00:08:05 +0300

    Refactor image-uploader tests to react-testing-library

--- resources/js/admin/__tests__/components/image-uploader.spec.js
+++ resources/js/admin/__tests__/components/image-uploader.spec.js
@@ -1,7 +1,33 @@
 import React from 'react'
-import {mount} from 'enzyme'
+import {fireEvent, render} from '@testing-library/react'
+import {toHaveAttribute} from '@testing-library/jest-dom'
 import ImageUploader from "../../components/image-uploader";
 
 describe('<ImageUploader/>', () => {
+    test('Given image prop is of type string, then img src will be image prop as is', () => {
+        const {getByAltText} = render(<ImageUploader image="/image/url"/>)
 
+        expect(getByAltText(/Mission image/i))
+            .toHaveAttribute('src', '/image/url')
+    })
+
+    test('Given image prop is of type File, then img src will be URL.createObjectUrl(image)', () => {
+        URL.createObjectURL = jest.fn(() => 'data://imageObjectUrl')
+        const image = new File([""], "imagename", {type: 'image/jpeg'})
+        const {getByAltText} = render(<ImageUploader image={image}/>)
+
+        expect(getByAltText(/Mission image/i))
+            .toHaveAttribute('src', 'data://imageObjectUrl')
+    })
+
+    test('URL.revokeObjectUrl called after img element loads', () => {
+        URL.createObjectURL = jest.fn(() => 'data://imageObjectUrl')
+        URL.revokeObjectURL = jest.fn()
+        const image = new File([""], "imagename", {type: 'image/jpeg'})
+        const {getByAltText} = render(<ImageUploader image={image}/>)
+
+        fireEvent.load(getByAltText(/Mission image/i))
+
+        expect(URL.revokeObjectURL).toHaveBeenCalledWith(URL.createObjectURL())
+    })
 })
--- resources/js/admin/components/image-uploader.js
+++ resources/js/admin/components/image-uploader.js
@@ -2,24 +2,25 @@ import React, {useRef, useMemo} from 'react'
 import defaultImage from "../../assets/images/philippe-unsplash.jpg"
 import ReactTooltip from "react-tooltip"
 
-function ImageUploader(props) {
+function ImageUploader({image, onImageChange, cssClass}) {
     const imageInput = useRef(null)
 
     function handleImageChange(e) {
         const file = e.target.files[0]
         if (file) {
-            props.onImageChange(file)
+            onImageChange(file)
         }
     }
 
     const imageSrc = useMemo(() => {
-        return props.image instanceof File
-            ? URL.createObjectURL(props.image)
-            : props.image
-    }, [props.image])
+        return image instanceof File
+            ? URL.createObjectURL(image)
+            : image
+
+    }, [image])
 
     return (
-        <div className={props.cssClass}>
+        <div className={cssClass}>
             <img
                 width="100%"
                 height="100%"
@@ -28,7 +29,7 @@ function ImageUploader(props) {
                 data-tip="Click to change image"
                 alt="Mission image"
                 style={{objectFit: 'cover'}}
-                onLoad={e => URL.revokeObjectURL(e.target.src)}
+                onLoad={e => URL.revokeObjectURL(e.target.src)} // This is called even when the src prop is not of an object url
             />
             <input
                 type="file"
